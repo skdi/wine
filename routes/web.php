c36992b254b3e4d71dba3b1f3e76ccf54aa87cf1
Skdi <jddhghf@gmail.com>
2018-10-26 18:07:33 -0500

    login_ver1.0

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('usuarios',function() 
+{
+	return view('usuarios');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
