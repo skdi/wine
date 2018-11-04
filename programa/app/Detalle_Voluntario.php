297c8b0b873b10c6646a29917222889052a07a3c
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2018-11-03 23:21:59 -0500

    Add files via upload

---
+++ programa/app/Detalle_Voluntario.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CruzRoja;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Detalle_Voluntario extends Model
+{
+	protected $table = 'detalle_voluntario';
+    protected $primaryKey = 'id_voluntario';
+}
---
+++ programa/app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CruzRoja\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
